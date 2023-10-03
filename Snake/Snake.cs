3dbf5c2ec9e10746542770060be9983682eb4596
Richard Pawson <rpawson@nakedobjects.net>
2023-10-03 11:30:59 +0100

    Snake, WordGame, and WordleSolver, initial attempts

---
+++ Snake/Snake.cs
@@ -0,0 +1,30 @@
+﻿public class Snake
+{
+    private Queue<Square> body = new Queue<Square>();
+    public Square head { get; private set; }
+    public Square tail()  =>  body.Peek();
+
+    public Snake(int x, int y, Direction startingDirection)
+    {
+        var tail = new Square(x, y);
+        body.Enqueue(tail);
+        head = tail.getAdjacentSquare(startingDirection);
+    }
+
+    public bool hasHitItself() => bodyCovers(head);
+
+    public bool bodyCovers(Square sq) => body.Any(seg => seg.isSameSquareAs(sq));
+
+    public void advanceHeadOneSquare(Direction d)
+    {
+        body.Enqueue(head);
+        head = head.getAdjacentSquare(d);
+    }
+
+    public void advanceTailOneSquare()
+    {
+        body.Dequeue();
+    }
+
+    public int Length() => body.Count;
+}
---
+++ Snake/Square.cs
@@ -0,0 +1,29 @@
+﻿public class Square
+{
+    public int x { get; init; }
+    public int y { get; init; }
+
+    public Square(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Square getAdjacentSquare(Direction d)
+    {
+        int newX = x;
+        int newY = y;
+        if (d == Direction.Left) newX -= 2;     // Horizontal adjacent is +/- 2 because a square is 2 display chars wide
+        else if (d == Direction.Right) newX += 2;
+        else if (d == Direction.Up) newY -= 1;
+        else if (d == Direction.Down) newY += 1;
+        return new Square(newX, newY);
+    }
+
+    public override string ToString() => $"{x},{y}";
+
+    public  bool isSameSquareAs(Square sq)
+    {
+        return x == sq.x && y == sq.y;
+    }
+}
